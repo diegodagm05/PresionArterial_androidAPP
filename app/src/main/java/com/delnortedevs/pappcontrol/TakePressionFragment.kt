87df96c372d2e71b23a488e192f0c43c7fd1b568
Axel Rodríguez <A01570152@itesm.mx>
2021-12-01 07:20:39 -0600

    Avances en pantalla de toma de presión
    Se agregó el método de la fecha y los campos para registrar presión.

--- app/src/main/java/com/delnortedevs/pappcontrol/TakePressionFragment.kt
+++ app/src/main/java/com/delnortedevs/pappcontrol/TakePressionFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import java.text.SimpleDateFormat
+import java.util.*
+import com.delnortedevs.pappcontrol.databinding.FragmentTakePressionBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,20 +24,34 @@ class TakePressionFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var _binding: FragmentTakePressionBinding? = null
+    private val binding get() = _binding!!
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-    }
+    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_take_pression, container, false)
+        _binding = FragmentTakePressionBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var fecha = "Hoy es: "
+        /*val format = SimpleDateFormat("dd/MM/yyyy")
+        val date = format.parse(Date().toString())*/
+        fecha = fecha.plus(Date().toString())
+        binding.tvFecha.text = fecha
     }
 
     companion object {
